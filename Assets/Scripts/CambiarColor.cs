b83c9ca8f5081e74a27da1b88866a1eec29d69ca
oscar gomez <oscarand.gomez@urosario.edu.co>
2020-11-06 22:51:32 -0500

    Placa y cambio de sprites

---
+++ Assets/Scripts/CambiarColor.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CambiarColor : MonoBehaviour
+{
+    public Sprite Color1;
+    public Sprite Color2;
+    public Sprite ColorGrid;
+    bool Color = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void CambioColor()
+    {
+        if(Color == true)
+        {
+            ColorGrid = this.GetComponent<SpriteRenderer>().sprite = Color1;
+            Color = false;
+        }
+        else
+        {
+            ColorGrid = this.GetComponent<SpriteRenderer>().sprite = Color2;
+            Color = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Pressure.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pressure : MonoBehaviour
+{
+    public GameObject Player;
+    private CambiarColor cambiarColor;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+     void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Placa1")
+        {
+            cambiarColor = FindObjectOfType<CambiarColor>();
+            cambiarColor.CambioColor();
+        }
+        
+    }
+}
